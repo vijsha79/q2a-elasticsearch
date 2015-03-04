72d2f8a46f65177f56481db94971bbac9a816f65
Vijay Sharma <sharmavijay79@gmail.com>
2015-03-04 00:45:09 -0800

    Updated the License Info

--- qa-plugin.php
+++ qa-plugin.php
@@ -2,14 +2,14 @@
 
 /*
 	Plugin Name: Elastic Search Support for Q2A
-	Plugin URI: https://github.com/NoahY/q2a-faq
-	Plugin Update Check URI: https://raw.github.com/NoahY/q2a-faq/master/qa-plugin.php
+	Plugin URI: https://github.com/vijsha79/q2a-elasticsearch
+	Plugin Update Check URI: https://raw.github.com/vijsha79/q2a-elasticsearch/master/qa-plugin.php
 	Plugin Description: Adds Support for Elastic Saarch for Q2A
-	Plugin Version: 0.4
+	Plugin Version: 0.1
 	Plugin Date: 2015-03-03
 	Plugin Author: Vijay Sharma
 	Plugin Author URI: http://www.question2answer.org/
-	Plugin License: GPLv2
+	Plugin License: MIT
 	Plugin Minimum Question2Answer Version: 1.3
 */
 
